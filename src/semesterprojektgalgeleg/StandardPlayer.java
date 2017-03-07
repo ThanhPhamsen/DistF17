112ee52a714daf830612cc98deab35454c86b05c
Andreas <Andreas@LAPTOP-AEKGAQKC.eduroam.wireless.dtu.dk>
2017-03-07 10:39:33 +0100

    Server og klient virker

--- src/semesterprojektgalgeleg/StandardPlayer.java
+++ src/semesterprojektgalgeleg/StandardPlayer.java
@@ -18,8 +18,8 @@ import java.util.Scanner;
  */
 public class StandardPlayer {
     public static void main(String[] args) throws MalformedURLException {
-        //URL url = new URL("http://ubuntu4.javabog.dk:9978/galgeleg?wsdl");
-          URL url = new URL("http://localhost:9978/galgeleg?wsdl");
+        URL url = new URL("http://ubuntu4.javabog.dk:9978/galgeleg?wsdl");
+          //URL url = new URL("http://localhost:9978/galgeleg?wsdl");
 		QName qname = new QName("http://server/", "GameLogicService");
                 Service service = Service.create(url, qname);
                 MainInterface i = service.getPort(MainInterface.class);
